52b70d75d66b1a41ba4ad46323d1cc18ece0d589
Matthew Oliver <matt@oliver.net.au>
2014-10-08 05:41:36 +0000

    Update builder files when they exist
    This change makes the swift_rings.py script update builder files if they
    exist. However there are some limitations:
    
    1. Once a ring has been defined with a partpower, this cannot be changed
       without rebuilding the ring. So rule is, pick a partpower based on
       future use of you swift cluster.
    
    2. When modifiying hosts using swift-builder you cannot change the
       region or zone. These are defined only when a new host is added. As
       this can cause major partition placement issues on existing hosts
       (will cause some major movement in the swift cluster).
    
       If you want to change a hosts zone or region, the best thing to do is
       set the wieght to 0 and let swift move data off the drive.. then
       remove it and add it again as a new host.
    
    NOTE: This needs testing and debugging, as I don't have a working full
          rpc environment yet.

--- rpc_deployment/roles/swift_ring_builder/templates/swift_rings.py
+++ rpc_deployment/roles/swift_ring_builder/templates/swift_rings.py
@@ -18,11 +18,14 @@ from optparse import OptionParser
 from os.path import exists
 from swift.cli.ringbuilder import main as rb_main
 
+import pickle
 import sys
 import threading
 import yaml
 
 USAGE = "usage: %prog -s <rpc_user_config.yml>"
+
+DEV_KEY = "%(ip)s:%(port)d/%(device)s"
 
 DEFAULT_REPL = {{ swift_default_replication_number }}
 DEFAULT_MIN_PART_HOURS = {{ swift_default_min_part_hours }}
@@ -55,9 +58,51 @@ DEFAULT_GROUPS= [
     'container'
 ]
 
-def create_buildfile(build_file, part_power, repl, min_part_hours):
-    run_and_wait(rb_main, ["swift-ring-builder", build_file, "create",
-                 part_power, repl, min_part_hours])
+def create_buildfile(build_file, part_power, repl, min_part_hours,
+                     update=False, data=None):
+    if update:
+        # build file exists, so lets just update the existing build file
+        if not data:
+            data = get_build_file_data(build_file)
+            if data is None:
+                data = {}
+
+        if repl != data.get('replicas'):
+            run_and_wait(rb_main, ["swift-ring-builder", build_file,
+                                   "set_replicas", repl])
+        if min_part_hours != data.get('min_part_hours'):
+            run_and_wait(rb_main, ["swift-ring-builder", build_file,
+                                   "set_min_part_hours", min_part_hours])
+        if part_power != data.get('part_power'):
+            print('Part power cannot be changed.. you must rebuild the ring "
+                  "if you need to change it.')
+
+    else:
+        run_and_wait(rb_main, ["swift-ring-builder", build_file, "create",
+                     part_power, repl, min_part_hours])
+
+def change_host_weight(build_file, host_search_str, weight):
+    run_and_wait(rb_main, ["swift-ring-builder", build_file, "set_weight",
+                 host_search_str, weight])
+
+def remove_host_from_ring(build_file, host):
+    run_and_wait(rb_main, ["swift-ring-builder", build_file, "remove",
+                 DEV_KEY % host])
+
+def update_host_in_ring(build_file, new_host, old_host):
+    r_ip = new_host.get('repl_ip', new_host['ip'])
+    r_port = new_host.get('repl_port', new_host['port'])
+    weight = new_host.get('weight', DEFAULT_HOST_WEIGHT)
+    if r_ip != old_host['replication_ip'] or \
+        r_port != old_host['replication_port']:
+        host_d = {'r_ip': r_ip, 'r_port': r_port}
+        host_d.update(host)
+        host_str = "%(ip)s:%(port)dR%(r_ip)s:%(d_port)d/%(name)s" % host_d
+        run_and_wait(rb_main, ["swift-ring-builder", build_file, "set_info",
+                               DEV_KEY % new_host, host_str])
+
+    if weight != old_host['weight']:
+        change_host_weight(build_file, DEV_KEY % new_host, weight)
 
 
 def add_host_to_ring(build_file, host):
@@ -67,14 +112,13 @@ def add_host_to_ring(build_file, host):
     host_str += "z%d" % (host.get('zone', DEFAULT_HOST_ZONE))
     host_str += "-%(ip)s:%(port)d" % host
     if host.get('repl_port'):
-        r_ip = host.get('repl_ip', host['host'])
+        r_ip = host.get('repl_ip', host['ip'])
         host_str += "R%s:%d" % (r_ip, host['repl_port'])
     host_str += "/%(name)s" % host
 
     weight = host.get('weight', DEFAULT_HOST_WEIGHT)
     run_and_wait(rb_main, ["swift-ring-builder", build_file, 'add',
                            host_str, str(weight)])
-
 
 def run_and_wait(func, *args):
     t = threading.Thread(target=func, args=args)
@@ -92,27 +136,58 @@ def check_section(conf, section):
         sys.exit(2)
 
 
+def get_build_file_data(build_file):
+    build_file_data = None
+    if exists(build_file):
+        try:
+            with open(build_file) as bf_stream:
+                build_file_data = pickle.load(bf_stream)
+        except Exception as ex:
+            print("Error: failed to load build file '%s': %s" % (build_file,
+                                                                 ex))
+            build_file_data = None
+    return build_file_data
+
+
 def build_ring(section, conf, part_power, hosts):
     # Create the build file
     build_file = "%s.builder" % (section)
+    build_file_data = get_build_file_data(build_file)
+
     repl = conf.get('repl_number', DEFAULT_REPL)
     min_part_hours = conf.get('min_part_hours',
                               DEFAULT_MIN_PART_HOURS)
-    create_buildfile(build_file, part_power, repl, min_part_hours)
+    update = build_file_data not None
+    create_buildfile(build_file, part_power, repl, min_part_hours, update,
+                     data=build_file_data)
 
+    old_hosts = {}
+    if update:
+        old_hosts = dict([(DEV_KEY % dev, i)
+                          for i, dev in enumerate(build_file_data['devs'])])
     section_key = section.split('-')[0]
     service_port = conf.get('port', DEFAULT_SECTION_PORT[section_key])
     for host in hosts:
         host_vars = hosts[host]
+        host_vars['device'] = host_vars['name']
         host_vars['port'] = service_port
         host_vars['groups'] = host_vars.get('groups', DEFAULT_GROUPS)
         if DEFAULT_GROUP_MAP[section] in host_vars['groups']:
-            add_host_to_ring(build_file, host_vars)
+            host_key = DEV_KEY % host_vars
+            if host_key in old_hosts:
+                old_host = build_file_data['devs'][old_hosts[host_key]]
+                update_host_in_ring(build_file, host_vars, old_host)
+                old_hosts.pop(host_key)
+            else:
+                add_host_to_ring(build_file, host_vars)
+
+    if old_hosts:
+        # There are still old hosts, these hosts must've been removed
+        for host in old_hosts:
+            remove_host_from_ring(build_file, host)
 
     # Rebalance ring
     run_and_wait(rb_main, ["swift-ring-builder", build_file, "rebalance"])
-    # rb_main(("swift-ring-builder", build_file, "rebalance"))
-
 
 def main(setup):
     # load the yaml file
@@ -126,7 +201,8 @@ def main(setup):
     _hosts = {}
     if _swift.get("swift_hosts"):
         for host in _swift['swift_hosts']:
-            host_vars = _swift['swift_hosts'][host]['container_vars']['swift_vars']
+            host_vars = \
+                _swift['swift_hosts'][host]['container_vars']['swift_vars']
             host_ip = host_vars.get('ip', _swift['swift_hosts'][host]['ip'])
             if not host_vars.get('drives'):
                 continue
@@ -134,13 +210,17 @@ def main(setup):
             for host_drive in host_drives:
                 host_drive['ip'] = host_drive.get('ip', host_ip)
                 if host_vars.get('groups'):
-                   host_drive['groups'] = host_drive.get('groups', host_vars['groups'])
+                   host_drive['groups'] = host_drive.get('groups',
+                                                         host_vars['groups'])
                 if host_vars.get('repl_ip'):
-                   host_drive['repl_ip'] = host_drive.get('repl_ip', host_vars['repl_ip'])
+                   host_drive['repl_ip'] = host_drive.get('repl_ip',
+                                                          host_vars['repl_ip'])
                 if host_vars.get('repl_port'):
-                   host_drive['repl_port'] = host_drive.get('repl_port', host_vars['repl_port'])
+                   host_drive['repl_port'] = \
+                       host_drive.get('repl_port', host_vars['repl_port'])
                 if host_vars.get('weight'):
-                   host_drive['weight'] = host_drive.get('weight', host_vars['weight'])
+                   host_drive['weight'] = host_drive.get('weight',
+                                                         host_vars['weight'])
                 key = "%s/%s" % (host_drive['ip'], host_drive['name'])
                 if key in _hosts:
                     print("%s already definined - duplicate device" % key)
@@ -155,12 +235,14 @@ def main(setup):
         return 1
     part_power = swift_vars.get('part_power')
 
-    # Create account ring - if the section is empty create an empty dict so defaults are used
+    # Create account ring - if the section is empty create an empty dict
+    # so defaults are used
     if not has_section(swift_vars, 'account'):
         swift_vars['account'] = {}
     build_ring('account', swift_vars['account'], part_power, _hosts)
 
-    # Create container ring - if the section is empty create an empty dict so defaults are use
+    # Create container ring - if the section is empty create an empty dict
+    # so defaults are used
     if not has_section(swift_vars, 'container'):
         swift_vars['container'] = {}
     build_ring('container', swift_vars['container'], part_power, _hosts)
