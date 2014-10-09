7b4ff7ade334b117c4378f8b48339b054afd83d4
Matthew Oliver <matt@oliver.net.au>
2014-10-09 16:15:36 +1100

    Add a validation step before any rings are touched
    This change changes the algorithm a little, the ring builder now
    gathers all the information about what rings to build and instead
    of calling build_ring straight away it adds the options for the call
    to a ring_calls list.
    Once all build calls are added to this list we loop through and call
    build_rings but with validate=True, which is like a dry run. When an
    error is detected a RingValidationError is raised.
    
    The errors its looking for is:
    
      - Modification of zone or region in existing drives.
      - Change of part_power.
      - And any exceptions raised while attempting to print integers.
    
    If this validation step passes (no error raised) the we loop and call
    build_ring again without the validate option.
    
    The benefit of this approach is all the rings are "validated" before
    any buildfiles are modified/created. So if a user gets an error we
    know the current state of the rings/builders are untouched.
    
    A validation error causes the script to return a 2. Seeing as this
    is >0 ansible _should_ see it as a failure.

--- rpc_deployment/roles/swift_ring_builder/templates/swift_rings.py
+++ rpc_deployment/roles/swift_ring_builder/templates/swift_rings.py
@@ -58,8 +58,11 @@ DEFAULT_GROUPS= [
     'container'
 ]
 
+class RingValidationError(Exception):
+    pass
+
 def create_buildfile(build_file, part_power, repl, min_part_hours,
-                     update=False, data=None):
+                     update=False, data=None, validate=False):
     if update:
         # build file exists, so lets just update the existing build file
         if not data:
@@ -67,17 +70,18 @@ def create_buildfile(build_file, part_power, repl, min_part_hours,
             if data is None:
                 data = {}
 
-        if repl != data.get('replicas'):
+        if repl != data.get('replicas') and not validate:
             run_and_wait(rb_main, ["swift-ring-builder", build_file,
                                    "set_replicas", repl])
-        if min_part_hours != data.get('min_part_hours'):
+        if min_part_hours != data.get('min_part_hours') and not validate:
             run_and_wait(rb_main, ["swift-ring-builder", build_file,
                                    "set_min_part_hours", min_part_hours])
         if part_power != data.get('part_power'):
-            print('Part power cannot be changed.. you must rebuild the ring '
-                  'if you need to change it.')
+            raise RingValidationError('Part power cannot be changed! '
+                                      'you must rebuild the ring if you need '
+                                      'to change it.')
 
-    else:
+    elif not validate:
         run_and_wait(rb_main, ["swift-ring-builder", build_file, "create",
                      part_power, repl, min_part_hours])
 
@@ -89,36 +93,53 @@ def remove_host_from_ring(build_file, host):
     run_and_wait(rb_main, ["swift-ring-builder", build_file, "remove",
                  host])
 
-def update_host_in_ring(build_file, new_host, old_host):
-    r_ip = new_host.get('repl_ip', new_host['ip'])
-    r_port = new_host.get('repl_port', new_host['port'])
-    weight = new_host.get('weight', DEFAULT_HOST_WEIGHT)
-    if r_ip != old_host['replication_ip'] or \
-        r_port != old_host['replication_port']:
-        host_d = {'r_ip': r_ip, 'r_port': r_port}
-        host_d.update(new_host)
-        host_str = "%(ip)s:%(port)dR%(r_ip)s:%(r_port)d/%(name)s" % host_d
-        run_and_wait(rb_main, ["swift-ring-builder", build_file, "set_info",
-                               DEV_KEY % new_host, host_str])
+def update_host_in_ring(build_file, new_host, old_host, validate=False):
+    if new_host.get('zone', 0) != old_host['zone']:
+        devstr = DEV_KEY % new_host
+        raise RingValidationError('Cannot update zone on %s, this can only be '
+                                  'done when the drive is added' % (devstr))
+    if new_host.get('region', 1) != old_host['region']:
+        devstr = DEV_KEY % new_host
+        raise RingValidationError('Cannot update region on %s, this can only '
+                                  'be done when the drive is added' % (devstr))
 
-    if weight != old_host['weight']:
+    try:
+        r_ip = new_host.get('repl_ip', new_host['ip'])
+        r_port = new_host.get('repl_port', new_host['port'])
+        weight = new_host.get('weight', DEFAULT_HOST_WEIGHT)
+        if r_ip != old_host['replication_ip'] or \
+            r_port != old_host['replication_port']:
+            host_d = {'r_ip': r_ip, 'r_port': r_port}
+            host_d.update(new_host)
+            host_str = "%(ip)s:%(port)dR%(r_ip)s:%(r_port)d/%(name)s" % host_d
+            if not validate:
+                run_and_wait(rb_main, ["swift-ring-builder", build_file,
+                                       "set_info", DEV_KEY % new_host,
+                                       host_str])
+    except Exception as ex:
+        raise RingValidationError(ex)
+
+    if weight != old_host['weight'] and not validate:
         change_host_weight(build_file, DEV_KEY % new_host, weight)
 
 
-def add_host_to_ring(build_file, host):
+def add_host_to_ring(build_file, host, validate=False):
     host_str = ""
-    if host.get('region') is not None:
-        host_str += 'r%(region)d' % host
-    host_str += "z%d" % (host.get('zone', DEFAULT_HOST_ZONE))
-    host_str += "-%(ip)s:%(port)d" % host
-    if host.get('repl_port'):
-        r_ip = host.get('repl_ip', host['ip'])
-        host_str += "R%s:%d" % (r_ip, host['repl_port'])
-    host_str += "/%(name)s" % host
-
-    weight = host.get('weight', DEFAULT_HOST_WEIGHT)
-    run_and_wait(rb_main, ["swift-ring-builder", build_file, 'add',
-                           host_str, str(weight)])
+    try:
+        if host.get('region') is not None:
+            host_str += 'r%(region)d' % host
+        host_str += "z%d" % (host.get('zone', DEFAULT_HOST_ZONE))
+        host_str += "-%(ip)s:%(port)d" % host
+        if host.get('repl_port'):
+            r_ip = host.get('repl_ip', host['ip'])
+            host_str += "R%s:%d" % (r_ip, host['repl_port'])
+        host_str += "/%(name)s" % host
+        weight = host.get('weight', DEFAULT_HOST_WEIGHT)
+    except Exception as ex:
+        raise RingValidationError(ex)
+    if not validate:
+        run_and_wait(rb_main, ["swift-ring-builder", build_file, 'add',
+                               host_str, str(weight)])
 
 def run_and_wait(func, *args):
     t = threading.Thread(target=func, args=args)
@@ -149,7 +170,7 @@ def get_build_file_data(build_file):
     return build_file_data
 
 
-def build_ring(section, conf, part_power, hosts):
+def build_ring(section, conf, part_power, hosts, validate=False):
     # Create the build file
     build_file = "%s.builder" % (section)
     build_file_data = get_build_file_data(build_file)
@@ -159,7 +180,7 @@ def build_ring(section, conf, part_power, hosts):
                               DEFAULT_MIN_PART_HOURS)
     update = build_file_data is not None
     create_buildfile(build_file, part_power, repl, min_part_hours, update,
-                     data=build_file_data)
+                           data=build_file_data, validate=validate)
 
     old_hosts = {}
     if update:
@@ -177,18 +198,20 @@ def build_ring(section, conf, part_power, hosts):
             host_key = DEV_KEY % host_vars
             if host_key in old_hosts:
                 old_host = build_file_data['devs'][old_hosts[host_key]]
-                update_host_in_ring(build_file, host_vars, old_host)
+                update_host_in_ring(build_file, host_vars, old_host,
+                                    validate=validate)
                 old_hosts.pop(host_key)
             else:
-                add_host_to_ring(build_file, host_vars)
+                add_host_to_ring(build_file, host_vars, validate=validate)
 
-    if old_hosts:
+    if old_hosts and not validate:
         # There are still old hosts, these hosts must've been removed
         for host in old_hosts:
             remove_host_from_ring(build_file, host)
 
     # Rebalance ring
-    run_and_wait(rb_main, ["swift-ring-builder", build_file, "rebalance"])
+    if not validate:
+        run_and_wait(rb_main, ["swift-ring-builder", build_file, "rebalance"])
 
 def main(setup):
     # load the yaml file
@@ -241,20 +264,21 @@ def main(setup):
     default_repl_num = swift_vars.get('repl_number', DEFAULT_REPL)
     default_min_part_hours = swift_vars.get('min_part_hours',
                                             DEFAULT_MIN_PART_HOURS)
+    ring_calls = []
 
     # Create account ring - if the section is empty create an empty dict
     # so defaults are used
     if not has_section(swift_vars, 'account'):
         swift_vars['account'] = {'repl_number': default_repl_num,
                                  'min_part_hours': default_min_part_hours}
-    build_ring('account', swift_vars['account'], part_power, _hosts)
+    ring_calls.append(('account', swift_vars['account'], part_power))
 
     # Create container ring - if the section is empty create an empty dict
     # so defaults are used
     if not has_section(swift_vars, 'container'):
         swift_vars['container'] = {'repl_number': default_repl_num,
                                   'min_part_hours': default_min_part_hours}
-    build_ring('container', swift_vars['container'], part_power, _hosts)
+    ring_calls.append(('container', swift_vars['container'], part_power))
 
     # Create object rings (storage policies)
     check_section(swift_vars, 'storage_policies')
@@ -276,7 +300,22 @@ def main(setup):
             policy['repl_number'] = default_repl_num
         if 'port' not in policy:
             policy['port'] = policy.get('port', DEFAULT_OBJECT_PORT)
-        build_ring(buildfilename, policy, part_power, _hosts)
+        ring_calls.append((buildfilename, policy, part_power))
+
+    # Now that we have gathered all the options for building/update the rings
+    # lets validate them
+    kargs = {'validate': True, 'hosts': _hosts}
+    for ring_call in ring_calls:
+        try:
+            build_rings(*ring_call, **kargs)
+        except RingValidationError as ex:
+            print ex
+            return 2
+
+    # If the validation passes lets go ahead and build the rings.
+    kargs.pop('validate')
+    for ring_call in ring_calls:
+        build_rings(*ring_call, **kargs)
 
 if __name__ == "__main__":
     parser = OptionParser(USAGE)
