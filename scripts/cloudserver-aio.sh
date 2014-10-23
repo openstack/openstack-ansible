bb99e77a2e520743effcb65000bba033d37d5088
Matt Thompson <mattt@defunct.ca>
2014-10-23 14:18:07 +0100

    Point mass_repo_version to 10.0.0rc1
    Currently we're pointed at v9.0.0, which doesn't work.

--- scripts/cloudserver-aio.sh
+++ scripts/cloudserver-aio.sh
@@ -228,7 +228,7 @@ maas_monitoring_zones:
   - mzord
   - mzlon
   - mzhkg
-maas_repo_version: v9.0.0
+maas_repo_version: 10.0.0rc1
 ## Neutron Options
 neutron_container_mysql_password: secrete
 neutron_service_password: secrete
