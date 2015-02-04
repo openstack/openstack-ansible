5db1a57c68730ea11a1874f8e9d9554dd2e30bc3
Hugh Saunders <hugh@wherenow.org>
2015-02-04 09:35:56 +0000

    Add multiple test lists to rpc_tempest_gate
    This patch enables the rpc_tempest_gate script to run different sets of
    tempest tests, depending on the supplied parameter.
    
    Partial-Bug: #1417973
    Change-Id: Icf395ad7fa76256519b5a2e4b97656fa2a3c3df9

--- rpc_deployment/roles/tempest/files/rpc_tempest_gate.sh
+++ rpc_deployment/roles/tempest/files/rpc_tempest_gate.sh
@@ -16,7 +16,59 @@
 # Script for running gate tests. Initially very sparse
 # additional projects and test types will be added over time.
 
+
+# -------------------- Shell Options -------------------------
 set -x
+
+
+# -------------------- Parameters -------------------------
+# The only parameter this script takes is the name of a test list to use:
+# ./$0 <test_list_name>
+#
+# If a name is not supplied commit_multinode will be used.
+
+test_list_name=${1:-commit_multinode}
+
+
+# -------------------- Functions -------------------------
+# Test list functions. Each tempest test scenario (eg commit gate, nightly,
+# pre release) should have a function here to generate the list of tests that
+# should be run. Each function takes in the full list of tempest tests and
+# should output a filtered list.
+
+gen_test_list_commit_multinode(){
+  # filter test list to produce list of tests to use.
+  egrep 'tempest\.api\.(identity|image|volume)'\
+    |grep -vi xml \
+    |grep -v compute \
+    |grep -v VolumesV.ActionsTest
+}
+
+# Run smoke tests
+gen_test_list_commit_aio(){
+  egrep 'test_minimum_basic|test_swift_basic_ops'
+}
+
+# Run smoke tests
+gen_test_list_nightly_heat_multinode(){
+  grep smoke
+}
+
+# Run all tests
+gen_test_list_all(){
+  cat
+}
+
+
+# -------------------- Main -------------------------
+
+available_test_lists=$(compgen -A function|sed -n '/^gen_test_list_/s/gen_test_list_//p')
+
+grep $test_list_name <<<$available_test_lists || {
+  echo "$test_list_name is not a valid test list, available test lists: "
+  echo $available_test_lists
+  exit 1
+}
 
 # work in tempest directory
 pushd /opt/tempest_*
@@ -24,18 +76,18 @@ pushd /opt/tempest_*
 # read creds into environment
 source /root/openrc
 
-# create testr trepo - required for testr to do anything
+# create testr repo - required for testr to do anything
 testr init &>/dev/null ||:
 
 # Get list of available tests
 testr list-tests > full_test_list
 
-# filter test list to produce list of tests to use.
-egrep 'tempest\.api\.(identity|image|volume)' < full_test_list \
-  |grep -vi xml \
-  |grep -v compute \
-  |grep -v VolumesV.ActionsTest \
-  > test_list
+# Write filter test list using selected function
+gen_test_list_$test_list_name <full_test_list >test_list
+
+test_list_summary="${test_list_name} ($(wc -l <test_list) tests)"
+
+echo "Using test list $test_list_summary"
 
 # execute chosen tests with pretty output
 ./run_tempest.sh --no-virtual-env -- --load-list test_list;
@@ -43,10 +95,9 @@ result=$?
 popd
 
 if [[ $result == 0 ]]; then
-  echo "GATE PASS"
+  echo "TEMPEST PASS $test_list_summary"
 else
-  echo "GATE FAIL"
+  echo "TEMPEST FAIL $test_list_summary"
 fi
 
 exit $result
-
