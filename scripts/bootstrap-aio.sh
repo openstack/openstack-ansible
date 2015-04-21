39bab4d683171e688a32d9437800d16e20b6b1e7
Kevin Carter <kevin.carter@rackspace.com>
2015-04-21 18:02:50 -0500

    Add release pinning to galera roles
    As both Ubuntu and the MariaDB repositories carry packages with the same
    names, apt package pinning is required to ensure that the packages we install
    come from the right repository. This patch adds a pinning preference file to
    ensure that the packages we install come from the MariaDB repository.
    
    Due to the OpenStack CI images having conflicting packages installed in the
    base image, this patch also includes the removal of these packages during the
    AIO bootstrap to ensure that the deployment succeeds.
    
    For environments where the same two packages are installed on the hosts prior
    to the deployment of os-ansible-deployment, they would need to ensure that the
    packages are removed first. This has been seen in environments where
    monitoring tooling has installed the packages as a dependancy.
    
    Also, the swift installation process requires access to the Ubuntu backports
    repository to install the liberasurecode1 and liberasurecode-dev packages. In
    the hpcloud environments used in OpenStack-CI the backport repository is not
    implemented, so we need to ensure that it's there if it hasn't already been
    configured. This patch does that as part of the AIO bootstrap process.
    
    Finally, this patch also updates the global requirements and the sha's for all
    openstack branches that we're pinned to.
    
    Closes-Bug: #1446772
    Closes-Bug: #1448152
    Change-Id: I5ad0b9010e49832c4f866c0b141e1c035d5f9268
    (cherry picked from commit 886fe01edb01b9f21e3576f50711738687b236d9)

--- scripts/bootstrap-aio.sh
+++ scripts/bootstrap-aio.sh
@@ -30,6 +30,7 @@ export TEMPEST_FLAT_CIDR=${TEMPEST_FLAT_CIDR:-"172.29.248.0/22"}
 export FLUSH_IPTABLES=${FLUSH_IPTABLES:-"yes"}
 export RABBITMQ_PACKAGE_URL=${RABBITMQ_PACKAGE_URL:-""}
 export SYMLINK_DIR=${SYMLINK_DIR:-"$(pwd)/logs"}
+export UBUNTU_REPO=$(awk "/^deb .*ubuntu $(lsb_release -sc) main/ {print \$2}" /etc/apt/sources.list)
 
 # Default disabled fatal deprecation warnings
 export CINDER_FATAL_DEPRECATIONS=${CINDER_FATAL_DEPRECATIONS:-"no"}
@@ -86,8 +87,19 @@ if [ ! "$(grep -e '^nameserver 8.8.8.8' -e '^nameserver 8.8.4.4' /etc/resolv.con
   echo -e '\n# Adding google name servers\nnameserver 8.8.8.8\nnameserver 8.8.4.4' | tee -a /etc/resolv.conf
 fi
 
-# Update the package cache and install required packages
+# Ensure that the ubuntu backports repo is in place
+# ref: https://bugs.launchpad.net/openstack-ansible/+bug/1448152
+if ! grep -qrni "^deb .* $(lsb_release -sc)-backports" /etc/apt/sources.list*; then
+  echo "deb ${UBUNTU_REPO} $(lsb_release -sc)-backports main restricted universe multiverse" > /etc/apt/sources.list.d/ubuntu-backports.list
+fi
+
+# Update the package cache
 apt-get update
+
+# Remove known conflicting packages in the base image
+apt-get purge -y libmysqlclient18 mysql-common
+
+# Install required packages
 apt-get install -y python-dev \
                    python2.7 \
                    build-essential \
@@ -281,7 +293,6 @@ echo 'galera_innodb_log_buffer_size: 32M' | tee -a /etc/openstack_deploy/user_va
 echo "required_kernel: $(uname --kernel-release)" | tee -a /etc/openstack_deploy/user_variables.yml
 
 # Set the Ubuntu apt repository used for containers to the same as the host
-UBUNTU_REPO=$(awk '/^deb .*ubuntu trusty main/ {print $2}' /etc/apt/sources.list)
 echo "lxc_container_template_main_apt_repo: ${UBUNTU_REPO}" | tee -a /etc/openstack_deploy/user_variables.yml
 echo "lxc_container_template_security_apt_repo: ${UBUNTU_REPO}" | tee -a /etc/openstack_deploy/user_variables.yml
 
