ff00d6ffd368f3712bf55db4b443859602b3eac8
Kevin Carter <kevin.carter@rackspace.com>
2016-01-13 14:05:08 -0600

    Added extra variable for upgrade
    This adds an additional variable to the delete me later variable
    file which will allow an upgrade to continue should the deployer
    not have access to the loadbalancer and can not setup/update vip
    addresses.
    
    Change-Id: I808a1f21c937ebf392ca9b874a26caf4947aa53b
    Closes-Bug: #1533817
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- scripts/upgrade-utilities/scripts/new-variable-prep.sh
+++ scripts/upgrade-utilities/scripts/new-variable-prep.sh
@@ -119,6 +119,12 @@ if ! grep -R '^openstack_repo_url\:' /etc/openstack_deploy/user_* /etc/openstack
     tee -a /etc/openstack_deploy/user_deleteme_post_upgrade_variables.yml
 fi
 
+# This pins the git repo url to the use the defined repo path -- Related issue https://bugs.launchpad.net/openstack-ansible/+bug/1533817
+if ! grep -R '^openstack_repo_git_url\:' /etc/openstack_deploy/user_* /etc/openstack_deploy/conf.d/; then
+  echo -e "openstack_repo_git_url: \"{{ openstack_repo_url }}/openstackgit\"" |\
+    tee -a /etc/openstack_deploy/user_deleteme_post_upgrade_variables.yml
+fi
+
 # Set the galera monitoring user to the old Juno Value.
 if ! grep '^galera_monitoring_user' /etc/openstack_deploy/user_deleteme_post_upgrade_variables.yml;then
   echo 'galera_monitoring_user: "haproxy"' | tee -a /etc/openstack_deploy/user_deleteme_post_upgrade_variables.yml
