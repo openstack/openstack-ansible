openstack/openstack-ansible

commit e9ef150728d181d1409b57d165a586704bff1f77

scripts: Introduce sources-branch-updater-lib library
Move core functionality from sources-branch-updater to
sources-branch-updater-lib. The reason for that is that external
projects may also want to use a variant of the existing script so it's
best if we move the common functionality to a library which can be
sourced by OSA and external scripts.

External projects may use OpenStack Ansible to deploy their
infrastructure. They may use a well-known OpenStack branch for that
(ie stable/ocata) or work directly from master. The latter is common
in the external CI provided by OPNFV. In that case, the OSA master
might be too bleeding edge so we would like to have the ability
to pin down specific SHAs for roles and packages even when we are
deploying from master.

Change-Id: Ief1b2bc8338f0c11c87bea28a9cf37d4397ab4f1

scripts/sources-branch-updater-lib.sh
@@ -1,0 +1,290 @@
+#!/usr/bin/env bash
+# Copyright 2015, Rackspace US, Inc.
+# Copyright 2017, SUSE LINUX GmbH.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###### HOOKS ########
+# The following hooks can be defined in the user scripts. All hooks accept the
+# following parameters:
+#
+# 1: repository name
+# 2: OpenStack service branch
+# 3: OSA branch
+# 4: repository address
+#
+#osa_pre_sync_hook: Actions to be performed before copying updated files to OSA OpenStack services repositories
+osa_pre_sync_hook() { true; }
+#osa_post_sync_hook: Actions to be performed after copying updated files to OSA OpenStack services repositories
+osa_post_sync_hook() { true; }
+
+###### HELPER FUNCTIONS ######
+osa_helper_cleanup_files() {
+  [[ $# > 0 ]] && rm -rf "${@}"
+}
+
+osa_helper_clone_os_package() {
+  local repo_name="${1}"
+  local os_branch="${2}"
+  local os_branch_sha"${3}"
+  local repo_address="${4}"
+  local os_repo_tmp_path="/tmp/os_${repo_name}"
+
+  osa_helper_cleanup_files ${os_repo_tmp_path}
+  # Do a shallow clone of the OpenStack repo to work with
+  if git clone --quiet --depth=10 --branch ${os_branch} --no-checkout --single-branch ${repo_address} ${os_repo_tmp_path}; then
+    pushd ${os_repo_tmp_path} > /dev/null
+    git checkout --quiet ${os_branch_sha}
+    popd > /dev/null
+    return 0
+  fi
+  return 1
+}
+
+osa_helper_clone_osa_role() {
+    local repo_name="${1}"
+    local osa_branch="${2}"
+    local osa_repo_address="${3:-https://git.openstack.org/openstack/openstack-ansible-os_${repo_name}}"
+    local osa_repo_tmp_path="/tmp/osa_${repo_name}"
+
+    osa_helper_cleanup_files ${osa_repo_tmp_path}
+    # Do a shallow clone of the OSA repo to work with
+    if git clone --quiet --depth=10 --branch ${osa_branch} --single-branch ${osa_repo_address} ${osa_repo_tmp_path}; then
+      pushd ${osa_repo_tmp_path} > /dev/null
+      git checkout --quiet origin/${osa_branch}
+      popd > /dev/null
+      return 0
+    fi
+    return 1
+}
+
+####### MAIN FUNCTIONS #######
+
+#
+# Updates SHAs for OSA roles and OpenStack repo packages.
+#
+sync_roles_and_packages() {
+  local repo_name repo_address branch_data branch_sha branch_entry osa_repo_tmp_path os_repo_tmp_path
+  local os_branch="${1}"; shift
+  local osa_branch="${1}"; shift
+  local service_file="${1}"; shift
+  local openstack_service_list="$@"
+  local osa_repo_address="https://git.openstack.org/openstack/openstack-ansible-os_${repo_name}"
+
+  IFS=$'\n'
+
+  # Iterate through the service file
+  for repo in $(grep 'git_repo\:' ${service_file}); do
+    # Set the repo name
+    repo_name=$(echo "${repo}" | sed 's/_git_repo\:.*//g')
+    local osa_repo_tmp_path="/tmp/osa_${repo_name}"
+    local os_repo_tmp_path="/tmp/os_${repo_name}"
+
+    echo -e "\nInspecting ${repo}..."
+
+    # Set the repo address
+    repo_address=$(echo ${repo} | awk '{print $2}')
+
+    # Get the branch data
+    branch_data=$(git ls-remote ${repo_address} | grep "${os_branch}$")
+
+    # If there is no branch data, move to the next role
+    [ -z "${branch_data}" ] && continue
+
+    # Set the branch sha for the head of the branch
+    branch_sha=$(echo "${branch_data}" | awk '{print $1}')
+
+    # Set the branch entry
+    branch_entry="${branch_sha} # HEAD of \"$os_branch\" as of $(date +%d.%m.%Y)"
+
+    # Write the branch entry into the repo_packages file
+    sed -i.bak "s|${repo_name}_git_install_branch:.*|${repo_name}_git_install_branch: $branch_entry|" ${service_file}
+
+    # If the repo is not in the specified list, then move to the next role
+    ! [[ "${openstack_service_list}" =~ "${repo_name}" ]] && continue
+
+    if osa_helper_clone_os_package ${repo_name} ${os_branch} ${branch_sha} ${repo_address}; then
+      if osa_helper_clone_osa_role ${repo_name} ${osa_branch}; then
+
+        # pre-sync user hook
+        osa_pre_sync_hook ${repo_name} ${os_branch} ${osa_branch} ${repo_address}
+
+        # Update the policy files
+        find ${os_repo_tmp_path}/etc -name "policy.json" -exec \
+          cp {} "${osa_repo_tmp_path}/templates/policy.json.j2" \;
+
+        # Tweak the paste files for any hmac key entries
+        find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
+          sed -i.bak "s|hmac_keys = SECRET_KEY|hmac_keys = {{ ${repo_name}_profiler_hmac_key }}|" {} \;
+
+        # Tweak the barbican paste file to support keystone auth
+        if [ "${repo_name}" = "barbican" ]; then
+          find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
+            sed -i.bak 's|\/v1\: barbican-api-keystone|\/v1\: {{ (barbican_keystone_auth \| bool) \| ternary('barbican-api-keystone', 'barbican_api') }}|'{} \;
+        fi
+
+        # Tweak the gnocchi paste file to support keystone auth
+        if [ "${repo_name}" = "gnocchi" ]; then
+          find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
+            sed -i.bak "s|pipeline = gnocchi+noauth|pipeline = {{ (gnocchi_keystone_auth \| bool) \| ternary('gnocchi+auth', 'gnocchi+noauth') }}|" {} \;
+        fi
+
+        # Update the paste files
+        find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
+          bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/templates/\$(basename \${name}).j2\"" \;
+
+        # Tweak the rootwrap conf filters_path (for neutron only)
+        if [ "${repo_name}" = "neutron" ]; then
+          find ${os_repo_tmp_path}/etc -name "rootwrap.conf" -exec \
+            sed -i.bak "s|filters_path=/etc/neutron|filters_path={{ ${repo_name}_conf_dir }}|" {} \;
+        fi
+
+        # Tweak the rootwrap conf exec_dirs
+        find ${os_repo_tmp_path}/etc -name "rootwrap.conf" -exec \
+          sed -i.bak "s|exec_dirs=|exec_dirs={{ ${repo_name}_bin }},|" {} \;
+
+        # Update the rootwrap conf files
+        find ${os_repo_tmp_path}/etc -name "rootwrap.conf" -exec \
+          cp {} "${osa_repo_tmp_path}/templates/rootwrap.conf.j2" \;
+
+        # Update the rootwrap filters
+        find ${os_repo_tmp_path}/etc -name "*.filters" -exec \
+          bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/files/rootwrap.d/\$(basename \${name})\"" \;
+
+        # Update the yaml files for Ceilometer
+        if [ "${repo_name}" = "ceilometer" ]; then
+          find ${os_repo_tmp_path}/etc -name "*.yaml" -exec \
+            bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/templates/\$(basename \${name}).j2\"" \;
+        fi
+
+        # Update the yaml files for Heat
+        if [ "${repo_name}" = "heat" ]; then
+          find ${os_repo_tmp_path}/etc -name "*.yaml" -exec \
+            bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/templates/\$(echo \${name} | rev | cut -sd / -f -2 | rev).j2\"" \;
+        fi
+
+        # post-sync user hook
+        post_sync_hook ${repo_name} ${os_branch} ${osa_branch} ${repo_address}
+
+        osa_helper_cleanup_files ${osa_repo_tmp_path} ${os_repo_tmp_path}
+      fi
+    fi
+
+    echo -e "Processed $repo_name @ $branch_entry\n"
+
+  done
+
+  unset IFS
+}
+
+#
+# Updates pip options using PIP_CURRENT_OPTIONS env variable
+#
+update_pip_options() {
+  # Update the PIP_INSTALL_OPTIONS with the current versions of pip, wheel and setuptools
+  PIP_CURRENT_OPTIONS=$(./scripts/get-pypi-pkg-version.py -p pip setuptools wheel -l horizontal)
+  sed -i.bak "s|^PIP_INSTALL_OPTIONS=.*|PIP_INSTALL_OPTIONS=\$\{PIP_INSTALL_OPTIONS:-'${PIP_CURRENT_OPTIONS}'\}|" scripts/scripts-library.sh
+
+  for pin in ${PIP_CURRENT_OPTIONS}; do
+    sed -i.bak "s|^$(echo ${pin} | cut -f1 -d=).*|${pin}|" global-requirement-pins.txt
+    sed -i.bak "s|^  - $(echo ${pin} | cut -f1 -d=).*|  - ${pin}|" group_vars/all/pip.yml
+  done
+
+  echo "Updated pip install options/pins"
+}
+
+#
+# Updates ansible-role-requirements file SHAs
+#
+update_ansible_role_requirements() {
+  local role_name role_version osa_repo_tmp_path role_git_sources
+  local osa_branch=${1}
+  local pre_release=${2}
+  # Update the ansible-role-requirements.yml file
+  # We don't want to be doing this for the master branch
+  if [ "${osa_branch}" != "master" ] || [ "${pre_release}" == "true" ]; then
+    echo "Updating ansible-role-requirements.yml"
+
+    if [ "${pre_release}" == "true" ]; then
+      role_git_sources=$(awk '/src: .*/ {print $2}' ansible-role-requirements.yml)
+    else
+      role_git_sources=$(awk '/src: .*\/openstack\// {print $2}' ansible-role-requirements.yml)
+    fi
+
+    # Loop through each of the role git sources, only looking for openstack roles
+    for role_src in ${role_git_sources}; do
+
+      # Determine the role's name
+      role_name=$(sed 's/^[ \t-]*//' ansible-role-requirements.yml | awk '/src: / || /name: / {print $2}' | grep -B1 "${role_src}" | head -n 1)
+      echo "... updating ${role_name}"
+
+      # If the role_src is NOT from git.openstack.org, try to get a tag first
+      if [[ ${role_src} != *"git.openstack.org"* ]]; then
+        role_version=$(git ls-remote --tags ${role_src} | awk '{print $2}' | grep -v '{}' | cut -d/ -f 3 | sort -n | tail -n 1)
+      fi
+
+      # Grab the latest SHA that matches the specified branch
+      if [[ -z "${role_version}" ]]; then
+        role_version=$(git ls-remote ${role_src} | grep "${OSA_BRANCH}$" | awk '{print $1}')
+      fi
+
+      # For OSA roles, get the release notes
+      if [[ ${role_src} == *"git.openstack.org"* ]]; then
+        local osa_repo_tmp_path="/tmp/osa_${role_name}"
+
+        osa_helper_clone_osa_role $role_name $osa_branch $role_src
+
+        # If there are releasenotes to copy, then copy them
+        if $(ls -1 ${osa_repo_tmp_path}/releasenotes/notes/*.yaml > /dev/null 2>&1); then
+          rsync -aq ${osa_repo_tmp_path}/releasenotes/notes/*.yaml releasenotes/notes/
+        fi
+
+        osa_helper_cleanu_files $osa_repo_tmp_path
+      fi
+
+      # Now use the information we have to update the ansible-role-requirements file
+      "$(dirname "${0}")/ansible-role-requirements-editor.py" -f ansible-role-requirements.yml -n "${role_name}" -v "${role_version}"
+
+      unset role_version
+    done
+    echo "Completed updating ansible-role-requirements.yml"
+  else
+    echo "Skipping the ansible-role-requirements.yml update as we're working on the master branch"
+  fi
+}
+
+update_release_version() {
+    local osa_branch=${1}
+    local service_file=${2}
+
+    # Update the release version in group_vars/all/all.yml
+    # We don't want to be doing this for the master branch and we only want
+    # to do it once, so we key off of a specific repo source file name.
+    if [[ "${osa_branch}" != "master" ]] && [[ "${service_file}" == "playbooks/defaults/repo_packages/openstack_services.yml" ]]; then
+
+      echo "Updating the release version..."
+      currentversion=$(awk '/openstack_release:/ {print $2}' group_vars/all/all.yml)
+
+      # Extract the required version info
+      major_version=$( echo ${currentversion} | cut -d. -f1 )
+      minor_version=$( echo ${currentversion} | cut -d. -f2 )
+      patch_version=$( echo ${currentversion} | cut -d. -f3 )
+
+      # increment the patch version
+      patch_version=$(( patch_version + 1 ))
+
+      sed -i .bak "s/${currentversion}/${major_version}.${minor_version}.${patch_version}/" group_vars/all/all.yml
+    else
+      echo "Skipping the release version update as we're working on the master branch"
+    fi
+}

scripts/sources-branch-updater.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copyright 2015, Rackspace US, Inc.
+# Copyright 2017, SUSE LINUX GmbH.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,7 +25,7 @@ SERVICE_FILE=${SERVICE_FILE:-"playbooks/defaults/repo_packages/openstack_service
 OPENSTACK_SERVICE_LIST=${OPENSTACK_SERVICE_LIST:-"$(grep 'git_repo\:' ${SERVICE_FILE} | awk -F '/' '{ print $NF }' | egrep -v 'requirements|-' | tr '\n' ' ')"}
 PRE_RELEASE=${PRE_RELEASE:-"false"}
 
-IFS=$'\n'
+source scripts/sources-branch-updater-lib.sh || { echo "Failed to source updater library"; exit 1; }
 
 if echo "$@" | grep -e '-h' -e '--help';then
     echo "
@@ -58,228 +59,42 @@ esac
 shift
 done
 
-# Iterate through the service file
-for repo in $(grep 'git_repo\:' ${SERVICE_FILE}); do
+commit_changes() {
+  local repo_name="${1}"
+  local osa_repo_tmp_path="/tmp/osa_${repo_name}"
 
-  echo -e "\nInspecting ${repo}..."
+  # Switch into the OSA git directory to work with it
+  pushd ${osa_repo_tmp_path} > /dev/null
 
-  # Set the repo name
-  repo_name=$(echo "${repo}" | sed 's/_git_repo\:.*//g')
-
-  # Set the repo address
-  repo_address=$(echo ${repo} | awk '{print $2}')
-
-  # Get the branch data
-  branch_data=$(git ls-remote ${repo_address} | grep "${OS_BRANCH}$")
-
-  # If there is branch data continue
-  if [ ! -z "${branch_data}" ];then
-
-    # Set the branch sha for the head of the branch
-    branch_sha=$(echo "${branch_data}" | awk '{print $1}')
-
-    # Set the branch entry
-    branch_entry="${branch_sha} # HEAD of \"$OS_BRANCH\" as of $(date +%d.%m.%Y)"
-
-    # Write the branch entry into the repo_packages file
-    sed -i.bak "s|${repo_name}_git_install_branch:.*|${repo_name}_git_install_branch: $branch_entry|" ${SERVICE_FILE}
-
-    # If the repo is in the specified list, then action the additional updates
-    if [[ "${OPENSTACK_SERVICE_LIST}" =~ "${repo_name}" ]]; then
-      os_repo_tmp_path="/tmp/os_${repo_name}"
-      osa_repo_tmp_path="/tmp/osa_${repo_name}"
-
-      # Ensure that the temp path doesn't exist
-      rm -rf ${os_repo_tmp_path} ${osa_repo_tmp_path}
-
-      # Do a shallow clone of the OpenStack repo to work with
-      if git clone --quiet --depth=10 --branch ${OS_BRANCH} --no-checkout --single-branch ${repo_address} ${os_repo_tmp_path}; then
-        pushd ${os_repo_tmp_path} > /dev/null
-          git checkout --quiet ${branch_sha}
-        popd > /dev/null
-
-        # Set the OSA address
-        osa_repo_address="https://git.openstack.org/openstack/openstack-ansible-os_${repo_name}"
-
-        # Do a shallow clone of the OSA repo to work with
-        if git clone --quiet --depth=10 --branch ${OSA_BRANCH} --single-branch ${osa_repo_address} ${osa_repo_tmp_path}; then
-          pushd ${osa_repo_tmp_path} > /dev/null
-            git checkout --quiet origin/${OSA_BRANCH}
-          popd > /dev/null
-
-          # Update the policy files
-          if [ "${repo_name}" != "keystone" ] && [ "${repo_name}" != "gnocchi" ] && [ "${repo_name}" != "ceilometer" ]; then
-            find ${os_repo_tmp_path}/etc -name "policy.json" -exec \
-              cp {} "${osa_repo_tmp_path}/templates/policy.json.j2" \;
-          fi
-
-          # Tweak the paste files for any hmac key entries
-          find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
-            sed -i.bak "s|hmac_keys = SECRET_KEY|hmac_keys = {{ ${repo_name}_profiler_hmac_key }}|" {} \;
-
-          # Tweak the barbican paste file to support keystone auth
-          if [ "${repo_name}" = "barbican" ]; then
-            find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
-              sed -i.bak 's|\/v1\: barbican-api-keystone|\/v1\: {{ (barbican_keystone_auth \| bool) \| ternary('barbican-api-keystone', 'barbican_api') }}|'{} \;
-          fi
-
-          # Update the paste files
-          if [ "${repo_name}" != "keystone" ] && [ "${repo_name}" != "gnocchi" ] && [ "${repo_name}" != "ceilometer" ]; then
-            find ${os_repo_tmp_path}/etc -name "*[_-]paste.ini" -exec \
-              bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/templates/\$(basename \${name}).j2\"" \;
-          fi
-
-          # Tweak the rootwrap conf filters_path (for neutron only)
-          if [ "${repo_name}" = "neutron" ]; then
-            find ${os_repo_tmp_path}/etc -name "rootwrap.conf" -exec \
-              sed -i.bak "s|filters_path=/etc/neutron|filters_path={{ ${repo_name}_conf_dir }}|" {} \;
-          fi
-
-          # Tweak the rootwrap conf exec_dirs
-          find ${os_repo_tmp_path}/etc -name "rootwrap.conf" -exec \
-            sed -i.bak "s|exec_dirs=|exec_dirs={{ ${repo_name}_bin }},|" {} \;
-
-          # Update the rootwrap conf files
-          find ${os_repo_tmp_path}/etc -name "rootwrap.conf" -exec \
-            cp {} "${osa_repo_tmp_path}/templates/rootwrap.conf.j2" \;
-
-          # Update the rootwrap filters
-          find ${os_repo_tmp_path}/etc -name "*.filters" -exec \
-            bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/files/rootwrap.d/\$(basename \${name})\"" \;
-
-          # Update the yaml files for Heat
-          if [ "${repo_name}" = "heat" ]; then
-            find ${os_repo_tmp_path}/etc -name "*.yaml" -exec \
-              bash -c "name=\"{}\"; cp \${name} \"${osa_repo_tmp_path}/templates/\$(echo \${name} | rev | cut -sd / -f -2 | rev).j2\"" \;
-          fi
-
-          # Switch into the OSA git directory to work with it
-          pushd ${osa_repo_tmp_path} > /dev/null
-
-            # Check for changed files
-            git_changed=$(git status --porcelain | wc -l)
-            # Check for untracked files
-            git_untracked=$(git ls-files --other --exclude-standard --directory | wc -l)
-            if [ ${git_untracked} -gt 0 ]; then
-              # If there are untracked files, ensure that the commit message includes
-              # a WIP prefix so that the patch is revised in more detail.
-              git_msg_prefix="[New files - needs update] "
-            else
-              git_msg_prefix=""
-            fi
-
-            # If any files have changed, submit a patch including the changes
-            if [ ${git_changed} -gt 0 ]; then
-              git checkout -b sha-update
-              git review -s > /dev/null
-              git add --all
-              git commit -a -m "${git_msg_prefix}Update paste, policy and rootwrap configurations $(date +%Y-%m-%d)" --quiet
-              git review > /dev/null
-            fi
-          popd > /dev/null
-        fi
-      fi
-
-      # Clean up the temporary files
-      rm -rf ${os_repo_tmp_path} ${osa_repo_tmp_path}
-    fi
-  fi
-
-  echo -e "Processed $repo_name @ $branch_entry\n"
-
-done
-
-unset IFS
-
-# Update the PIP_INSTALL_OPTIONS with the current versions of pip, wheel and setuptools
-PIP_CURRENT_OPTIONS=$(./scripts/get-pypi-pkg-version.py -p pip setuptools wheel -l horizontal)
-sed -i.bak "s|^PIP_INSTALL_OPTIONS=.*|PIP_INSTALL_OPTIONS=\$\{PIP_INSTALL_OPTIONS:-'${PIP_CURRENT_OPTIONS}'\}|" scripts/scripts-library.sh
-
-for pin in ${PIP_CURRENT_OPTIONS}; do
-  sed -i.bak "s|^$(echo ${pin} | cut -f1 -d=).*|${pin}|" global-requirement-pins.txt
-  sed -i.bak "s|^  - $(echo ${pin} | cut -f1 -d=).*|  - ${pin}|" group_vars/all/pip.yml
-done
-
-echo "Updated pip install options/pins"
-
-# Update the ansible-role-requirements.yml file
-# We don't want to be doing this for the master branch
-if [ "${OSA_BRANCH}" != "master" ] || [ "${PRE_RELEASE}" == "true" ]; then
-  echo "Updating ansible-role-requirements.yml"
-
-  if [ "${PRE_RELEASE}" == "true" ]; then
-    ROLE_GIT_SOURCES=$(awk '/src: .*/ {print $2}' ansible-role-requirements.yml)
+  # Check for changed files
+  git_changed=$(git status --porcelain | wc -l)
+  # Check for untracked files
+  git_untracked=$(git ls-files --other --exclude-standard --directory | wc -l)
+  if [ ${git_untracked} -gt 0 ]; then
+    # If there are untracked files, ensure that the commit message includes
+    # a WIP prefix so that the patch is revised in more detail.
+    git_msg_prefix="[New files - needs update] "
   else
-    ROLE_GIT_SOURCES=$(awk '/src: .*\/openstack\// {print $2}' ansible-role-requirements.yml)
+    git_msg_prefix=""
   fi
 
-  # Loop through each of the role git sources, only looking for openstack roles
-  for role_src in ${ROLE_GIT_SOURCES}; do
+  # If any files have changed, submit a patch including the changes
+  if [ ${git_changed} -gt 0 ]; then
+    git checkout -b sha-update
+    git review -s > /dev/null
+    git add --all
+    git commit -a -m "${git_msg_prefix}Update paste, policy and rootwrap configurations $(date +%Y-%m-%d)" --quiet
+    git review > /dev/null
+  fi
+  popd > /dev/null
+}
 
-    # Determine the role's name
-    role_name=$(sed 's/^[ \t-]*//' ansible-role-requirements.yml | awk '/src: / || /name: / {print $2}' | grep -B1 "${role_src}" | head -n 1)
-    echo "... updating ${role_name}"
+osa_post_sync_hook() { commit_changes "$@"; }
 
-    # If the role_src is NOT from git.openstack.org, try to get a tag first
-    if [[ ${role_src} != *"git.openstack.org"* ]]; then
-      role_version=$(git ls-remote --tags ${role_src} | awk '{print $2}' | grep -v '{}' | cut -d/ -f 3 | sort -n | tail -n 1)
-    fi
+sync_roles_and_packages ${OS_BRANCH} ${OSA_BRANCH} ${SERVICE_FILE} ${OPENSTACK_SERVICE_LIST}
 
-    # Grab the latest SHA that matches the specified branch
-    if [[ -z "${role_version}" ]]; then
-      role_version=$(git ls-remote ${role_src} | grep "${OSA_BRANCH}$" | awk '{print $1}')
-    fi
+update_pip_options
 
-    # For OSA roles, get the release notes
-    if [[ ${role_src} == *"git.openstack.org"* ]]; then
-      # Setup a var for tmp space
-      osa_repo_tmp_path="/tmp/osa_${role_name}"
+update_ansible_role_requirements ${OSA_BRANCH} ${PRE_RELEASE}
 
-      # Ensure that the temp path doesn't exist
-      rm -rf ${osa_repo_tmp_path}
-
-      # Do a shallow clone of the repo to work with
-      git clone --quiet --depth=10 --branch ${OSA_BRANCH} --single-branch ${role_src} ${osa_repo_tmp_path}
-      pushd ${osa_repo_tmp_path} > /dev/null
-        git checkout --quiet origin/${OSA_BRANCH}
-      popd > /dev/null
-
-      # If there are releasenotes to copy, then copy them
-      if $(ls -1 ${osa_repo_tmp_path}/releasenotes/notes/*.yaml > /dev/null 2>&1); then
-        rsync -aq ${osa_repo_tmp_path}/releasenotes/notes/*.yaml releasenotes/notes/
-      fi
-
-      # Clean up the temporary files
-      rm -rf ${osa_repo_tmp_path}
-    fi
-
-    # Now use the information we have to update the ansible-role-requirements file
-    "$(dirname "${0}")/ansible-role-requirements-editor.py" -f ansible-role-requirements.yml -n "${role_name}" -v "${role_version}"
-
-    unset role_version
-  done
-  echo "Completed updating ansible-role-requirements.yml"
-else
-  echo "Skipping the ansible-role-requirements.yml update as we're working on the master branch"
-fi
-
-# Update the release version in group_vars/all/all.yml
-# We don't want to be doing this for the master branch and we only want
-# to do it once, so we key off of a specific repo source file name.
-if [[ "${OSA_BRANCH}" != "master" ]] && [[ "${SERVICE_FILE}" == "playbooks/defaults/repo_packages/openstack_services.yml" ]]; then
-
-  echo "Updating the release version..."
-  currentversion=$(awk '/openstack_release:/ {print $2}' group_vars/all/all.yml)
-
-  # Extract the required version info
-  major_version=$( echo ${currentversion} | cut -d. -f1 )
-  minor_version=$( echo ${currentversion} | cut -d. -f2 )
-  patch_version=$( echo ${currentversion} | cut -d. -f3 )
-
-  # increment the patch version
-  patch_version=$(( patch_version + 1 ))
-
-  sed -i .bak "s/${currentversion}/${major_version}.${minor_version}.${patch_version}/" group_vars/all/all.yml
-else
-  echo "Skipping the release version update as we're working on the master branch"
-fi
+update_release_version ${OSA_BRANCH} ${SERVICE_FILE}

commit ebdf81b3dc1922210de422ce78de7c0ebee10f3d

scripts: sources-branch-updater-lib.sh: Fix typos
Commit e9ef150728d1 ("scripts: Introduce sources-branch-updater-lib
library") introduced the sources-branch-updater-lib.sh library which
can be used by external scripts to do their own tagging. However, this
also introduced a few typos. 'osa_helper_cleanu_files' should be
'osa_helper_cleanup_files' and 'OSA_BRANCH' should be 'osa_branch'

Fixes: e9ef150728d1 ("scripts: Introduce sources-branch-updater-lib library")
Change-Id: I4c5c7320b038c5c0f75cffa47c82f74bd50ee9bc

scripts/sources-branch-updater-lib.sh
@@ -235,7 +235,7 @@ update_ansible_role_requirements() {
 
       # Grab the latest SHA that matches the specified branch
       if [[ -z "${role_version}" ]]; then
-        role_version=$(git ls-remote ${role_src} | grep "${OSA_BRANCH}$" | awk '{print $1}')
+        role_version=$(git ls-remote ${role_src} | grep "${osa_branch}$" | awk '{print $1}')
       fi
 
       # For OSA roles, get the release notes
@@ -249,7 +249,7 @@ update_ansible_role_requirements() {
           rsync -aq ${osa_repo_tmp_path}/releasenotes/notes/*.yaml releasenotes/notes/
         fi
 
-        osa_helper_cleanu_files $osa_repo_tmp_path
+        osa_helper_cleanup_files $osa_repo_tmp_path
       fi
 
       # Now use the information we have to update the ansible-role-requirements file