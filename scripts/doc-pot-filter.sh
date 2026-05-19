a557da61cea0f90683cdc7f73c086874956d867c
Dmitriy Chubinidze <dcu995@gmail.com>
2026-05-19 19:48:52 +0000

    docs: fix deploy-guide pot files merging into single file
    Concatenate all pot files for deploy guide into a single deploy-guide.pot,
    and place it into doc/source/locale/ alongside main docs.
    
    Change-Id: Ia44a9d0970c1bccdc31c278d56568e2b7b8640e9
    Signed-off-by: Dmitriy Chubinidze <dcu995@gmail.com>

--- scripts/doc-pot-filter.sh
+++ scripts/doc-pot-filter.sh
@@ -6,3 +6,8 @@ set -e
 sphinx-build -b gettext \
     doc/source/deploy-guide/source \
     doc/build/gettext-deploy-guide/
+
+# Concatenate all deploy-guide pot files into a single one
+# and store in doc/source/locale/
+msgcat --use-first --sort-by-file doc/build/gettext-deploy-guide/*.pot \
+    > doc/build/gettext/deploy-guide.pot
