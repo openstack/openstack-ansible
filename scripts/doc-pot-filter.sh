f8884d19433ac7ff17deced6574763c0ca017770
Dmitriy Chubinidze <dcu995@gmail.com>
2026-06-24 21:42:41 +0000

    docs: remove stale deploy-guide pot files after merging
    Without cleanup, individual pot files in the temporary build directory
    were being picked up by Zanata as separate documents.
    
    Change-Id: I434e2f0dae9a82592232b5aa363cfddca4017055
    Signed-off-by: Dmitriy Chubinidze <dcu995@gmail.com>

--- scripts/doc-pot-filter.sh
+++ scripts/doc-pot-filter.sh
@@ -11,3 +11,7 @@ sphinx-build -b gettext \
 # and store in doc/source/locale/
 msgcat --use-first --sort-by-file doc/build/gettext-deploy-guide/*.pot \
     > doc/build/gettext/deploy-guide.pot
+
+# Remove the temp directory so its individual pot files
+# are not picked up and pushed to Zanata
+rm -rf doc/build/gettext-deploy-guide/
